ee70b2a8ddbe1a54e16732109ba1ca4c4a613141
PutthidaSR <psamrith@uw.edu>
2020-05-18 23:52:54 -0700

    updated bottom tab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
 import Routes from './routes';
 
 /**
@@ -11,54 +10,8 @@ class App extends React.Component {
   }
 
   render() {
-    console.log('test');
-    return (
-      <SafeAreaView>
-        <View>
-          <Text>Hello, world!</Text>
-        </View>
-      </SafeAreaView>
-    );
+    return <Routes />;
   }
 }
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: 'white',
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: 'white',
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: 'white',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: 'white',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: 'white',
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable comma-dangle */
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
 
 import Homepage from './components/screens/Homepage';
+import WorldScreen from './components/screens/WorldScreen';
+import HelpScreen from './components/screens/HelpScreen';
 
 /**
  * This class represents the main layout of the application.
@@ -16,7 +21,6 @@ export default class Routes extends React.Component {
 
   render() {
 
-    console.log('this.props.isUserSignedIn', this.props.isUserSignedIn);
     const Stack = createStackNavigator();
 
     return (
@@ -27,14 +31,72 @@ export default class Routes extends React.Component {
           }}
           initialRouteName={Homepage}
         >
-          <Stack.Screen name="Home" component={Homepage} />
-        {/**<Stack.Screen name="Signin" component={SignInScreen} />
-          <Stack.Screen name="Signup" component={SignUpScreen} />
-          <Stack.Screen name="Confirmation" component={ConfirmationScreen} />
-          <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />**/}
-
+          <Stack.Screen name="Home" component={BottomTab} />
         </Stack.Navigator>
       </NavigationContainer>
     );
   }
+}
+
+/**
+ * Bottom navigator tab for Homepage
+ */
+const MainBottomTab = createBottomTabNavigator();
+
+function BottomTab() {
+  return (
+    <MainBottomTab.Navigator
+      initialRouteName="Home"
+      tabBarOptions= {{
+        activeTintColor: '#2980b9',
+        backgroundColor: 'blue',
+        inactiveTintColor: 'rgba(0,0,0,0.3)',
+        labelStyle:{fontWeight: 'bold'},
+        labelPosition: 'below-icon',
+        style:{padding: 5, backgroundColor: 'white'}
+      }}
+    >
+      <MainBottomTab.Screen
+        name="Home"
+        component={Homepage}
+        options={{
+          tabBarLabel: 'Cambodia',
+          tabBarIcon: () => (
+            <Image
+              style={{ width: 30, height: 25 }}
+              source={require('./assets/images/cambodia-icon.png')}
+            />
+          )
+        }}
+      />
+
+      <MainBottomTab.Screen
+        name="WorldScreen"
+        component={WorldScreen}
+        options={{
+          tabBarLabel: 'World Data',
+          tabBarIcon: () => (
+            <Image
+              style={{ width: 25, height: 25 }}
+              source={require('./assets/images/globe-icon.png')}
+            />
+          )
+        }}
+      />
+
+      <MainBottomTab.Screen
+        name="HelpScreen"
+        component={HelpScreen}
+        options={{
+          tabBarLabel: 'More Info',
+          tabBarIcon: () => (
+            <Image
+              style={{ width: 40, height: 40 }}
+              source={require('./assets/images/help-icon.png')}
+            />
+          )
+        }}
+      />
+    </MainBottomTab.Navigator>
+  );
 }
